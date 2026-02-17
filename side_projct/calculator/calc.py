db6152d32a65002ca68ae65470dec6da8d2c23a8
jaafaralireeabi <159414760+jaafaralireeabi@users.noreply.github.com>
2026-02-18 01:17:31 +0300

    calc commit

--- side_projct/calculator/calc.py
+++ side_projct/calculator/calc.py
@@ -1,1 +1,27 @@
+# calculator آلة  حاسبة
 
+"""
+          1-  تعريف متغيرين بأسم a,b على سبيل المثال لا الحصر
+    2- واخذ قيمتهما من المستخدم input from user
+3- تعريف متغير لكل عملية حسابية )...,sum,sub,mul,div)
+4- عرض نتائج كل عملية
+
+"""
+
+a = float(input("enter the first number: "))
+b = float(input("enter the second number: "))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a % b
+
+"""
+ يمكنك اضافة عمليات حسابية اخرى
+مختلفة حاول ان تجرب ذلك )مثل باقي القسمة او الاس او غير ذلك(
+"""
+
+print("a + b = ", sum)
+print("a - b = ", sub)
+print("a * b = ", mul)
+print("a / b = ", div)
